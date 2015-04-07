a7fd8bf10f6382d82961955913bb38a590e861fc
Matt Rosenthal <rmatthew@student.ethz.ch>
2015-04-07 14:11:58 -0400

    try ignoring arts and preps

--- ConstitutionReader.java
+++ ConstitutionReader.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 
 public class ConstitutionReader {
 	
@@ -10,24 +11,43 @@ public class ConstitutionReader {
 		int word = 0; 
 		long charbyte = 0;
 		
+		int pline = 0;
+		int pword = 0;
+		long pcharbyte = 0;
+		
+		HashSet<String> ignore = new HashSet<String>();
+		String list = "I We You They a and the that of for with";
+		for (String s : list.split(" ")) {
+			ignore.add(s);
+		}
+		
 		try {
-			File file = new File(args[0]);
+			File file = new File("constitution.txt");
 			charbyte = file.length();
 			
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			String currline = null;
+			
 			while ((currline = reader.readLine()) != null) {
 				line++;
 				String[] words = currline.split(" +");
 				if (words.length == 1) {
-					if (!words[0].equals("")) word += words.length;
+					if (!words[0].equals("")) {
+						word += words.length;
+						if (!ignore.contains(words[0])) pword++;
+					}
 				}
 				else {
 					word += words.length;
+					for (String tok : words) {
+						tok = tok.replaceAll("[^a-zA-Z]", "");
+						if (!ignore.contains(tok)) pword++;
+					}
 				}
 			}
 			
-			System.out.println(line + " " + word + " " + charbyte);
+			System.out.println("all: " + line + " " + word + " " + charbyte);
+			System.out.println("proper: " + pword);
 			reader.close();
 			
 		}
